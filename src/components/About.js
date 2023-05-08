c26cdfc967c9c1aa64a2606a6aebbea1eaf46736
NIKHITHA <chundurunikhitha55@gmail.com>
2023-05-08 12:07:36 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,35 @@
+import aboutImg from '../images/about.jpeg'
+import Title from './Title'
+
+const About = () => {
+    return (
+        <section className="section" id="about">
+        <Title title = 'about' subTitle= 'us' />     
+
+      <div className="section-center about-center">
+        <div className="about-img">
+          <img
+            src={aboutImg}
+            className="about-photo"
+            alt="awesome beach"
+          />
+        </div>
+        <article className="about-info">
+          <h3>explore the difference</h3>
+          <p>
+            The customer himself, the customer will be able to 
+            pursue the adipiscing of the company. Is any one of these aspersed?
+          </p>
+          <p>
+            The customer himself, the customer will be able to pursue the adipiscing of the company. It is roughened
+            For any of these times I will explain the pains with a wise hatred
+            whence pain?
+          </p>
+          <a href="#" className="btn">read more</a>
+        </article>
+      </div>
+    </section>
+    )
+}
+
+export default About;
---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+const Hero = () => {
+    return(
+        <section className="hero" id="home">
+      <div className="hero-banner">
+        <h1>continue exploring</h1>
+        <p>
+          The company itself is a very successful company. It is said that I will explain the debts of my life.
+        </p>
+        <a href="#tours" className="btn hero-btn">explore tours</a>
+      </div>
+    </section>
+    )
+}
+
+export default Hero;
